4324efe178ee9cce892e96e8898efa3d8ce99fd7
Sparrow Xue <sparrowxue@Sparrows-MacBook-Pro.local>
2020-02-16 05:15:44 +0000

    Opened the app on notification click

--- app/src/main/java/com/notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/notification/MyFirebaseMessagingService.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
+
+import android.app.PendingIntent;
 import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -42,12 +44,16 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void raiseNotification(String title, String body){
+        PendingIntent contentIntent =
+                PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
         String channelId  = getString(R.string.default_notification_channel_id);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.ic_stat_ic_notification)
                 .setContentTitle(title)
                 .setContentText(body)
-                .setPriority(NotificationCompat.PRIORITY_HIGH);
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(contentIntent);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
